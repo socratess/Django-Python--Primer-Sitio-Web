fe386be50bfc8c7b24c5c86e9f02be0292a15298
Socrates Palacios Moreno <socratespalaciosmoreno@gmail.com>
2022-12-02 01:37:25 -0500

    Update views.py

--- generator/views.py
+++ generator/views.py
@@ -29,29 +29,3 @@ def password(request) :
            
           
     return render(request, 'generator/password.html',{'password': generated_password})
-
-
-def passwords(request) :
-    
-    characters = list('abcdefghijklmnopqrstuvwxyz') 
-    generated_password = ''
-    
-    length = int(request.GET.get('length'))
-    
-    if request.GET.get('uppercase'):
-             characters.extend(list('ABCEFGHIJKLMNOPQRSTUVWXYZ'))
-    if request.GET.get('special'):
-             characters.extend(list('+-*/,.#$%&/()=?¡@!°'))
-    if request.GET.get('numbers'):
-             characters.extend(list('1234567890'))         
-    print("hello")
-    print("hello1")
-    print("hello")
-    print("hello1")
-    
-           
-    for x in range(length):
-        generated_password += random.choice(characters)
-           
-          
-    return render(request, 'generator/password.html',{'password': generated_password})
